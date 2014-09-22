a2dfa9fe236b16091c672986f7badeee99789165
Walter Moreira <walter@waltermoreira.net>
2014-09-22 10:58:29 -0500

    Pass distro parameter to build_app

--- app_builder/app_builder_image/build_app.sh
+++ app_builder/app_builder_image/build_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USAGE="\
-build_app.sh <name>:         provision app 'name' from roles
+build_app.sh <name> <distro>:         provision app 'name' from roles
 build_app.sh commit <name>:  create image
 "
 
@@ -12,10 +12,11 @@ usage() {
 
 start_ssh_server() {
     name=$1;
+    distro=$2;
     state=$(docker inspect -f "{{ .State.Running }}" $name 2>/dev/null)
     if [ ! "$?" -eq 0 -o "$state" == "<no value>" ]; then
         # No container with than name. Start one.
-        docker run -d -P --name $name ssh_server
+        docker run -d -P --name $name ${distro}_ssh_server
     elif [ "$state" == "false" ]; then
         # The container is stopped. Restart it.
         docker start $name;
@@ -27,6 +28,7 @@ start_ssh_server() {
 if [ "$#" -eq 0 -o "$1" == "-h" ]; then
     usage;
 elif [ "$1" == "commit" ]; then
+    # --- commit
     docker commit $2 $2
     docker rm -f $2
     (
@@ -34,8 +36,9 @@ elif [ "$1" == "commit" ]; then
         cat Runfile
     ) | docker build -t $2 -
 else
-    start_ssh_server $1;
-    docker run --rm ssh_server cat /etc/ssh/ssh_host_rsa_key > key
+    # --- provision
+    start_ssh_server $1 $2;
+    docker run --rm ${2}_ssh_server cat /etc/ssh/ssh_host_rsa_key > key
     chmod 0600 key
     docker run -it --rm \
         -v $(pwd)/roles:/build/roles \
